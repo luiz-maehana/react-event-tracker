8d79fb5b6f0ab6c8672c6f6bdd900be6bea5822b
Luiz Fernando Maehana <luizmaehana@gmail.com>
2024-02-10 20:27:00 -0300

    Refactor onUpdateEvent

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -2,10 +2,11 @@
 import React from 'react'
 import style from './Calendario.module.scss';
 import ptBR from './localizacao/ptBR.json'
-import Kalend, { CalendarView } from 'kalend'
+import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from 'kalend'
 import 'kalend/dist/styles/index.css';
 import { useRecoilValue } from 'recoil';
 import { listaDeEventosState } from '../state/atom';
+import useAtualizarEvento from '../state/hooks/useAtualizarEvento';
 
 interface IKalendEvento {
   id?: number
@@ -18,8 +19,8 @@ interface IKalendEvento {
 const Calendario: React.FC = () => {
 
   const eventosKalend = new Map<string, IKalendEvento[]>();
-
   const eventos = useRecoilValue(listaDeEventosState)
+  const atualizarEvento = useAtualizarEvento()
 
   eventos.forEach(evento => {
     const chave = evento.inicio.toISOString().slice(0, 10)
@@ -34,6 +35,22 @@ const Calendario: React.FC = () => {
       color: 'blue'
     })
   })
+
+  const onEventDragFinish: OnEventDragFinish = (
+    kalendEventoInalterado: CalendarEvent,
+    kalendEventoAtualizado: CalendarEvent,
+  ) => {
+    const evento = eventos.find(e => e.id === kalendEventoAtualizado.id)
+    if (evento) {
+      const eventoAtualizado = {
+        ...evento
+      }
+      eventoAtualizado.inicio = new Date(kalendEventoAtualizado.startAt)
+      eventoAtualizado.fim = new Date(kalendEventoAtualizado.endAt)
+      atualizarEvento(evento)
+    }
+  }
+
   return (
     <div className={style.Container}>
       <Kalend
@@ -46,6 +63,7 @@ const Calendario: React.FC = () => {
         calendarIDsHidden={['work']}
         language={'customLanguage'}
         customLanguage={ptBR}
+        onEventDragFinish={onEventDragFinish}
       />
     </div>
   );
